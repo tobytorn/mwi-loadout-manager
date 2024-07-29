161ea95ce3b8e944923c185b6a94c82c7c2b7a35
tobytorn <tobytorn@milkywayidle.com>
2024-07-29 20:24:51 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    greasemonkey: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+};
